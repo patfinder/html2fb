45614c4563298655ac67e22eb595fb6dd4bb330e
Vuong Nguyen <vuong.se@gmail.com>
2024-06-01 16:06:32 -0400

    .

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # html2fb
 Convert HTML to Facebook post format
+
+
+# Algorithm
+
+- Start from root node
+- Traverse all current node. If there are formatter, push to the format-stack
+- For each node, traverse all it child nodes
+- If this is leaf node, write output
+    - Combine all the formatters when writing the output
+    - Supported formatter: list (ordered, unordered), bold, itatic, indent (?)
+
--- test.py
+++ test.py
@@ -1,9 +1,22 @@
 import codecs
 from bs4 import BeautifulSoup
 
-with codecs.open('./data/giao-tiep.html', mode='r', encoding="utf-8") as file1:
-    html_doc = file1.read()
 
-soup = BeautifulSoup(html_doc, 'html.parser')
+class Formetter():
+    def __init__(self) -> None:
+        # b, i, bi: bold, itatic, bold and itatic
+        # o, u: ordered, unordered list
+        self.type = None
 
-print(f'P Node: {soup.div}')
+
+def main():
+    with codecs.open('./data/giao-tiep.html', mode='r', encoding="utf-8") as file1:
+        html_doc = file1.read()
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    print(f'P Node: {soup.div}')
+
+
+# run main
+main()
